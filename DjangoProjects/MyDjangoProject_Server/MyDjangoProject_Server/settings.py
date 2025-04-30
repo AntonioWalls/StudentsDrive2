f9f391a5a3f4a835f9b425bb66658832b12f6fba
Antoniowalls <120619779+AntonioWalls@users.noreply.github.com>
2025-04-29 21:28:39 -0700

    changes to isolate auth

--- DjangoProjects/MyDjangoProject_Server/MyDjangoProject_Server/settings.py
+++ DjangoProjects/MyDjangoProject_Server/MyDjangoProject_Server/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users'
+    'users', #Modulo de usuarios
+    'authentication' #Modulo de autenticación
 ]
 
 MIDDLEWARE = [
