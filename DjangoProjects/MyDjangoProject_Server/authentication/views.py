78992bf826b6141462d1c341fe0e79763bb1bc9e
Antoniowalls <120619779+AntonioWalls@users.noreply.github.com>
2025-04-30 21:47:03 -0700

    implementation of roles

--- DjangoProjects/MyDjangoProject_Server/authentication/views.py
+++ DjangoProjects/MyDjangoProject_Server/authentication/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from roles.models import Role
+from roles.serializers import RoleSerializer
 from users.serializers import UserSerializer
 from users.models import User, UserHasRoles
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -22,25 +23,58 @@ def register(request):
         client_role = get_object_or_404(Role, id = 'CLIENT')
         UserHasRoles.objects.create(id_user=user, id_rol = client_role)
         
-        return Response(serializer.data, status = status.HTTP_201_CREATED)
-    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        roles = Role.objects.filter(userhasroles__id_user=user)
+        roles_serializer = RoleSerializer(roles, many=True)
+        response_data={
+            **serializer.data,
+            'roles': roles_serializer.data
+        }
+        
+        return Response(response_data, status = status.HTTP_201_CREATED)
+    #manejaremos los errores de esta manera:
+    error_messages=[]
+    for field, errors in serializer.errors.items():
+        for error in errors:
+            error_messages.append(f"{field}: {error}")
+            
+    error_response = {
+        "message": error_messages,
+        "statusCode":status.HTTP_400_BAD_REQUEST
+    }
+    
+    return Response(error_response, status = status.HTTP_400_BAD_REQUEST)
  #Metodo POST el cual valida el login del usuario por medio de validaciones
 @api_view(['POST'])
 def login(request):
     email = request.data.get('email')
     password = request.data.get('password')
-    
+     
     if not email or not password:
-        return Response({"error": "El email y el password son obligatorios"}, status = status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {
+                "message": "El email y el password son obligatorios",
+                "statusCode": status.HTTP_400_BAD_REQUEST
+            },
+            status = status.HTTP_400_BAD_REQUEST
+        )
     try: 
         user = User.objects.get(email = email)
     except User.DoesNotExist:
-        return Response({"error": "El email o el password no son validos"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            {
+                "message": "El email y el password no son validos",
+                "statusCode": status.HTTP_401_UNAUTHORIZED
+            },
+            status = status.HTTP_401_UNAUTHORIZED
+        )
     
     #Aquí se hace una comparación de los password encriptado y el no encriptado
     if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
         refresh_token = RefreshToken.for_user(user)
         access_token = str(refresh_token.access_token) #aquí se genera el token de autenticación único para cada usuario
+        roles = Role.objects.filter(userhasroles__id_user=user)
+        roles_serializer = RoleSerializer(roles, many=True)
+        
         user_data= {
             "user": {
                 "id": user.id,
@@ -50,9 +84,17 @@ def login(request):
                 "phone": user.phone,
                 "image": user.phone,
                 "notification_token": user.notification_token,
+                "roles": roles_serializer.data,
             },
             "token": 'Bearer ' + access_token
         }
         return Response(user_data, status = status.HTTP_200_OK)
     else:
-        return Response({"error": "El email o el password no son validos"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            {
+                "message": "El email y el password no son validos",
+                "statusCode": status.HTTP_401_UNAUTHORIZED
+            },
+            status = status.HTTP_401_UNAUTHORIZED
+        )
+ 
